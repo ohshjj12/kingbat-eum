6e7dc539bebc1f8a2825f8a3b3e8f077b1e6dc74
honghongk <porgreany@gmail.com>
2026-08-25 16:05:15 +0900

    Allow production host in Vite server

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,10 @@ import aitDevtools from "@apps-in-toss/devtools/unplugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [aitDevtools.vite(), react()],
+  server: {
+    allowedHosts: ['kingbat-eum.kpearl.net'],
+  },
+  preview: {
+    allowedHosts: ['kingbat-eum.kpearl.net'],
+  },
 })
